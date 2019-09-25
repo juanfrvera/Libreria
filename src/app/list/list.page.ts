0c134d22373b783ab92b26a18af6631601cd5093
Juan <juanfrvera@gmail.com>
2019-09-25 18:09:10 -0300

    No se que toqué

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -19,13 +19,15 @@ export class ListPage implements OnInit {
     'bluetooth',
     'build'
   ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
+  public items: Array<{ title: string; autor:string; precio:string; stock: number; }> = [];
   constructor() {
     for (let i = 1; i < 11; i++) {
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+        title: 'Libro ' + i,
+        autor: 'Sandro',
+        precio: '$'+(i*10),
+        stock: i,
+        //icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
   }
