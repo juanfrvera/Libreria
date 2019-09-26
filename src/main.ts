dcf2f1346420a27c81b77e09ba1dada72b513514
Juan <juanfrvera@gmail.com>
2019-09-26 15:02:30 -0300

    Preparando para hacer andar typeorm

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,29 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+createConnection({
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "juan",
+  password: "juan",
+  database: "libreria",
+  entities: [
+    "./entity/*.ts"
+  ],
+  synchronize: true,
+  logging: false
+}).then(async connection => {
+  alert("hola");
+}).catch(error => console.log(error));
