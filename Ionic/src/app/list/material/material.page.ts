a4156da1c738939c2bf399a4bfba0f1ebf747c24
Juan <juanfrvera@gmail.com>
2019-10-04 18:40:02 -0300

    Agregado al carrito en modal de material

--- Ionic/src/app/list/material/material.page.ts
+++ Ionic/src/app/list/material/material.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
 import { Material } from 'src/model/Material';
+import { VariablesService } from 'src/services/Variables';
 
 @Component({
   selector: 'material-page',
@@ -8,16 +9,23 @@ import { Material } from 'src/model/Material';
   styleUrls: ['material.page.scss']
 })
 export class MaterialPage implements OnInit {
-  item : Material;
+  item: Material;
 
-  constructor(private modalController: ModalController, private navParams: NavParams) {
-
-  }
+  constructor(private modalController: ModalController, private navParams: NavParams, private variables : VariablesService) {}
+  
   ngOnInit() {
     this.item = this.navParams.data.item;
   }
   async closeModal() {
     const onClosedData: string = "Wrapped Up!";
     await this.modalController.dismiss(onClosedData);
+  }
+  AgregarACarrito(material: Material) {
+    material.Carrito++;
+    this.variables.Carrito.Aumentar(material.Id);
+  }
+  Reducir(material: Material) {
+    material.Carrito--;
+    this.variables.Carrito.Reducir(material.Id);
   }
 }
